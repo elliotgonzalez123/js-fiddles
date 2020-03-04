124a1c149cd7c1e3d764a8402676e608dc268257
Elliot Gonzalez <elliotgrey@gmail.com>
2020-03-04 16:35:34 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const sortColumns = str => {
+  let obj = [];
+  let regex = /\r?\n|\r/g;
+  let newStr = str.replace(regex, ',').split(',');
+  let nums = [];
+
+  for (let i = 0; i < newStr.length; i++) {
+    let name = newStr[i];
+
+    if (parseInt(name) !== parseInt(name)) {
+      obj.push({ name });
+    }
+  }
+  for (let i = obj.length; i < newStr.length; i += obj.length) {
+    let elem = newStr.slice(i, i + obj.length);
+
+    nums.push(elem);
+  }
+
+  obj.map((item, idx) => {
+    item['val'] = nums.map(num => num[idx]);
+  });
+
+  obj = obj.sort((a, b) => {
+    return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+  });
+
+  let output = '';
+  obj.map(item => {
+    output += item.name + ',';
+  });
+
+  output += '\n';
+  let i = 0;
+  let j = 0;
+  while (i < nums.length) {
+    obj.map(item => {
+      output += `${item.val[j]},`;
+    });
+    output += '\n';
+    j++;
+    i++;
+  }
+  return output.slice(0, -2);
+};
+
+console.log(
+  sortColumns(
+    'daryl,Andrew,arnold,Yamuna,zayne,Sally\n2465,21789,123636,15241,1987,5296\n12,16,13,14,11,15'
+  )
+);
