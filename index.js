e0df1b2425b705178935edbac9c0169d02ef6d22
Elliot Gonzalez <elliotgrey@gmail.com>
2020-03-04 18:36:32 -0500

    ttt

--- index.js
+++ index.js
@@ -49,3 +49,67 @@ console.log(
     'daryl,Andrew,arnold,Yamuna,zayne,Sally\n2465,21789,123636,15241,1987,5296\n12,16,13,14,11,15\n120,160,130,140,110,150\n1200,1600,1300,1400,1100,1500'
   )
 );
+
+const whoWonTheRaceMrCoder = arr => {
+  let obj = [];
+  let dnfCount = 0;
+
+  arr.map(item => {
+    if (item.time === 'dnf') {
+      dnfCount++;
+    }
+  });
+
+  if (dnfCount === arr.length) {
+    return 'There is no winner';
+  }
+
+  if (arr.length - dnfCount === 1) {
+    let winnerByDNF = [];
+    arr.map(item => {
+      if (item.time !== 'dnf') {
+        winnerByDNF.push(item);
+      }
+    });
+    return `${winnerByDNF[0].name} won by no contest`;
+  }
+
+  dnfCount = 0;
+
+  arr.map(item => {
+    obj.push({ name: item.name, time: new Date('03/04/2020 ' + item.time) });
+  });
+
+  obj.sort((a, b) => a.time - b.time);
+
+  let margin = obj[1].time - obj[0].time;
+  let time = new Date(margin);
+
+  return `${
+    obj[0].name
+  } won by 0 hours, ${time.getMinutes()} minutes, and ${time.getSeconds()} seconds`;
+};
+
+console.log(
+  whoWonTheRaceMrCoder([
+    { name: 'Elizabeth', time: '05:42:14' },
+    { name: 'Bernie', time: '05:12:53' },
+    { name: 'Joe', time: 'dnf' }
+  ])
+);
+
+console.log(
+  whoWonTheRaceMrCoder([
+    { name: 'Pete', time: 'dnf' },
+    { name: 'Amy', time: '05:12:53' },
+    { name: 'Mike', time: 'dnf' }
+  ])
+);
+
+console.log(
+  whoWonTheRaceMrCoder([
+    { name: 'George', time: 'dnf' },
+    { name: 'Abraham', time: 'dnf' },
+    { name: 'Ulysses', time: 'dnf' }
+  ])
+);
